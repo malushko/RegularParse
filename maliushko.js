a510566cf5fce5e4a1391c8db3f471138d77529b
Vadim Malushko <man.malushko@gmail.com>
2017-02-01 14:17:06 +0200

    Update maliushko.js

--- maliushko.js
+++ maliushko.js
@@ -1,8 +1,10 @@
 
 var addresses = require('./addresses');
+
     let regular = /\d[0-9-а-яА-Яё]{1,30}\s?[А-Яа-яё]+|([А-Я][ёА-Яа-я-\s]{1,50})|\d+/g;
     let arrayObjectParse = [];
     for(var i = 0; i < addresses.length; i++){
+        let arrayMatch = [];
         let regularObject = {};
         while (match = regular.exec(addresses[i])){
             arrayMatch.push(match[0]);
@@ -19,7 +21,7 @@ var addresses = require('./addresses');
         let jsonRegularObject = JSON.stringify(regularObject);
         arrayObjectParse.push(jsonRegularObject);
     }
-
+console.log(arrayObjectParse);
 module.exports = arrayObjectParse;
 
 // ... some good code ...
